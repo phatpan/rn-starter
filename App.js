163aa2420d836ae44c5d4cc3fe28dcd90ecc30b6
phatcharaphan <phat.pan@gmail.com>
2019-08-31 15:35:53 +0700

    create image screen

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import { createStackNavigator, createAppContainer } from 'react-navigation';
 import HomeScreen from './src/screens/HomeScreen';
 import ComponetsScreen from './src/screens/ComponentsScreen';
 import ListScreen from './src/screens/ListScreen';
+import ImageScreen from './src/screens/ImageScreen';
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
     Components: ComponetsScreen,
-    List: ListScreen
+    List: ListScreen,
+    Image: ImageScreen
   },
   {
     initialRouteName: 'Home',
