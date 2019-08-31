91a3bf7a10035d12440896c2199b35cae1b44220
phatcharaphan <phat.pan@gmail.com>
2019-08-31 14:18:16 +0700

    create list screen

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import HomeScreen from './src/screens/HomeScreen';
 import ComponetsScreen from './src/screens/ComponentsScreen';
+import ListScreen from './src/screens/ListScreen';
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
-    Components: ComponetsScreen
+    Components: ComponetsScreen,
+    List: ListScreen
   },
   {
     initialRouteName: 'Components',
--- src/screens/ComponentsScreen.js
+++ src/screens/ComponentsScreen.js
@@ -2,18 +2,18 @@ import React from 'react';
 import { Text, StyleSheet, View } from 'react-native';
 
 const ComponetsScreen = () => {
-    const greeting = 'Bye there!';
+    const name = 'Kae';
     return (
         <View>
-            <Text style={styles.textStyle}>This is the components screen</Text>
-            <Text style={styles.subHeaderStyle}>{greeting}</Text>
+            <Text style={styles.textStyle}>Getting started with React Native!</Text>
+            <Text style={styles.subHeaderStyle}>My name is {name}</Text>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     textStyle: {
-        fontSize: 30
+        fontSize: 45
     },
     subHeaderStyle: {
         fontSize: 20
