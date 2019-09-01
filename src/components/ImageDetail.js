e118982cad1bc9cc63991cf9c4192ff762b180a7
phatcharaphan <phat.pan@gmail.com>
2019-09-01 14:11:54 +0700

    display image

--- src/components/ImageDetail.js
+++ src/components/ImageDetail.js
@@ -2,12 +2,12 @@ import React from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
 
 const ImageDetail = (props) => {
-    console.log(props);
     return (
         <View>
             <Image source={props.imageSource}></Image>
             <Text>{props.title}</Text>
-            </View>);
+            <Text>Image Score - {props.score}</Text>
+        </View>);
 };
 
 const styles = StyleSheet.create({});
--- src/screens/ImageScreen.js
+++ src/screens/ImageScreen.js
@@ -5,9 +5,9 @@ import ImageDetail from '../components/ImageDetail';
 const ImageScreen = () => {
     return (
         <View>
-            <ImageDetail title="Forest" imageSource={require('../../assets/forest.jpg')}></ImageDetail>
-            <ImageDetail title="Beach" imageSource={require('../../assets/beach.jpg')}></ImageDetail>
-            <ImageDetail title="Mountain" imageSource={require('../../assets/mountain.jpg')}></ImageDetail>
+            <ImageDetail title="Forest" imageSource={require('../../assets/forest.jpg')} score={9}></ImageDetail>
+            <ImageDetail title="Beach" imageSource={require('../../assets/beach.jpg')} score={7}></ImageDetail>
+            <ImageDetail title="Mountain" imageSource={require('../../assets/mountain.jpg')} score={4}></ImageDetail>
         </View>
     )
 };
