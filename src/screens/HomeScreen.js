b7ebd06bebc40a66e7247d4f29d9270c020b7cb9
phatcharaphan <phat.pan@gmail.com>
2019-09-01 21:18:49 +0700

    create color screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import { Text, StyleSheet, View, Button, TouchableOpacity } from 'react-native';
 
-const HomeScreen = ({navigation}) => {
+const HomeScreen = ({ navigation }) => {
   return (
     <View>
       <Text style={styles.text}>Hello world</Text>
       <Button
-        onPress={()=> navigation.navigate('Components')}
+        onPress={() => navigation.navigate('Components')}
         title="Go to Components Demo"
       />
       <Button
         title="Go to List Demo"
-        onPress={()=> navigation.navigate('List')}
+        onPress={() => navigation.navigate('List')}
       />
-       <Button
+      <Button
         title="Go to Image Demo"
-        onPress={()=> navigation.navigate('Image')}
+        onPress={() => navigation.navigate('Image')}
       />
       <Button
         title="Go to Counter Demo"
-        onPress={()=> navigation.navigate('Counter')}
+        onPress={() => navigation.navigate('Counter')}
+      />
+      <Button
+        title="Go to Color Demo"
+        onPress={() => navigation.navigate('Color')}
       />
     </View>);
 };
