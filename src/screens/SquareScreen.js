16f22028781ce627761bc9a2dbd1400058e8a4e1
phatcharaphan <phat.pan@gmail.com>
2019-09-04 22:13:31 +0700

    create squere page

---
+++ src/screens/SquareScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const SquareScreen = () => {
+    return <Text>Square Screen</Text>
+};
+
+const styleSheet = StyleSheet.create({});
+
+export default SquareScreen;
